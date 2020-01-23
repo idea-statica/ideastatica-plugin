d5fcdf4bd5e329f9070cfda4cbf4c7b1f7b8084e
mpospisil76 <m.pospisil76@gmail.com>
2020-01-23 18:32:36 +0100

    multitarget

--- IdeaStatiCa.Plugin/MemberHiddenClientFactory.cs
+++ IdeaStatiCa.Plugin/MemberHiddenClientFactory.cs
@@ -11,7 +11,7 @@ namespace IdeaStatiCa.Plugin
 		MemberHiddenCheckClient Create();
 	}
 
-	public class MemberHiddenClientFactory : IDisposable, IMemberCalculatorFactory
+	public class MemberHiddenClientFactory : IMemberCalculatorFactory
 	{
 		private readonly string IdeaInstallDir;
 		Process CalculatorProcess { get; set; }
@@ -92,41 +92,5 @@ namespace IdeaStatiCa.Plugin
 			CalculatorProcess = null;
 			CalculatorUrl = null;
 		}
-
-		#region IDisposable Support
-		private bool disposedValue = false; // To detect redundant calls
-
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
-					// TODO: dispose managed state (managed objects).
-				}
-
-				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-				// TODO: set large fields to null.
-
-				disposedValue = true;
-			}
-		}
-
-		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-		// ~MemberHiddenClientFactory()
-		// {
-		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-		//   Dispose(false);
-		// }
-
-		// This code added to correctly implement the disposable pattern.
-		public void Dispose()
-		{
-			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-			Dispose(true);
-			// TODO: uncomment the following line if the finalizer is overridden above.
-			// GC.SuppressFinalize(this);
-		}
-		#endregion
 	}
 }
